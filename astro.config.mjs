89f1ada7024bc2bfb8c999cebdc4cea972640d9e
akazdayo <82073147+akazdayo@users.noreply.github.com>
2026-07-15 18:05:52 +0900

    icon類を全部astro-iconsにした

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,27 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import icon from "astro-icon";
 
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [
+    icon({
+      include: {
+        "simple-icons": [
+          "nixos",
+          "rust",
+          "typescript",
+          "python",
+          "linux",
+          "vim",
+          "vrchat",
+          "twitter",
+        ],
+      },
+    }),
+  ],
   vite: {
     plugins: [tailwindcss()],
   },
